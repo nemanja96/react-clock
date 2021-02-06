a8ce3c44ec31311e4da57bd2e1c7ecaf938452ce
Nemanja Radivojevic <nemanjaradivojevic1603@gmail.com>
2021-02-06 18:19:21 +0100

    deploy clock app on netlify

--- src/components/clock.jsx
+++ src/components/clock.jsx
@@ -1,57 +1,48 @@
-import React, { PureComponent } from 'react';
+import React, { PureComponent } from "react";
 
 export class Clock extends React.Component {
-    state = { 
-        date: new Date()
-    }
+  state = {
+    date: new Date(),
+  };
 
-    render() { 
-        return (
-            <div className="clock">
-                <div className="item">
-                    <h1>{this.dayInWeek()}</h1>
-                    <span>Day</span>
-                </div>
-                <div className="item">
-                    <h1>{this.state.date.getHours()}</h1>
-                    <span>Hours</span>
-                </div>
-                <div className="item">
-                    <h1>{this.state.date.getMinutes()}</h1>
-                    <span>Minutes</span>
-                </div>
-                <div className="item">
-                    <h1>{this.state.date.getSeconds()}</h1>
-                    <span>Seconds</span>
-                </div>
-            </div>    
-        );
-    }
+  render() {
+    return (
+      <div className="clock">
+        <div className="item">
+          <h1>{this.dayInWeek()}</h1>
+          <span>Day</span>
+        </div>
+        <div className="item">
+          <h1>{this.state.date.getHours()}</h1>
+          <span>Hours</span>
+        </div>
+        <div className="item">
+          <h1>{this.state.date.getMinutes()}</h1>
+          <span>Minutes</span>
+        </div>
+        <div className="item">
+          <h1>{this.state.date.getSeconds()}</h1>
+          <span>Seconds</span>
+        </div>
+      </div>
+    );
+  }
 
-    componentDidMount() {
-        const refreshTime = 100;
-        this.intervalID = setInterval( () => {
-            this.setState({ date: new Date() });
-        }, refreshTime);
-    }
+  componentDidMount() {
+    const refreshTime = 100;
+    this.intervalID = setInterval(() => {
+      this.setState({ date: new Date() });
+    }, refreshTime);
+  }
 
-    // Clear interval when the clock is unmounted
-    componentWillUnmount() {
-        clearInterval(this.intervalID);
-    }
+  // Clear interval when the clock is unmounted
+  componentWillUnmount() {
+    clearInterval(this.intervalID);
+  }
 
-    dayInWeek() {
-        let day = this.state.date.getDay();
-        let days =
-            [
-                "Su",
-                "Mo",
-                "Tu",
-                "We",
-                "Th",
-                "Fr",
-                "Sa"
-            ];
-        return days[day];
-    }
+  dayInWeek() {
+    let day = this.state.date.getDay();
+    let days = ["Su", "Mo", "Tu", "We", "Th", "Fr", "Sa"];
+    return days[day];
+  }
 }
